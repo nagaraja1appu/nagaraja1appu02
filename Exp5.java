b37a52b658038fe49bd905cddd0b0536f7407a71
nagaraja1appu <135014248+nagaraja1appu@users.noreply.github.com>
2023-05-30 06:36:04 -0300

    Add files via upload

---
+++ Exp5.java
@@ -0,0 +1,13 @@
+class Exp5
+{
+          public static void main(String s[])
+           {
+                int n=1;
+                int sum=0;
+                while(n<=10);
+               {
+                       sum=sum+n;
+                        n++;
+               }
+          }
+}
